7729daed69eac05b06c55c1c2a4b26dd60ec2fce
Hasnaa Turk <alhasnaa.tu@gmail.com>
2020-12-22 14:19:22 +0200

    fixed login form

--- Fawran/resources/views/auth/login.blade.php
+++ Fawran/resources/views/auth/login.blade.php
@@ -1,7 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container">
+<div class="content">
+  <div class="container-fluid">
   @if ($errors->any())
           <div class="alert alert-danger">
               <ul>
@@ -13,14 +14,19 @@
       @endif
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="card mt-5">
 
+                <div class="card-header card-header-rose card-header-icon">
+                  <div class="card-icon">
+                    <i class="material-icons">contacts</i>
+                  </div>
+                  <h4 class="card-title">login</h4>
+                </div>
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" class="form-horizontal" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        <div class="row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">user name</label>
 
                             <div class="col-md-6">
@@ -34,7 +40,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class=" row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
@@ -48,7 +54,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class=" row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -60,7 +66,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class=" row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
@@ -78,5 +84,6 @@
             </div>
         </div>
     </div>
+  </div>
 </div>
 @endsection
